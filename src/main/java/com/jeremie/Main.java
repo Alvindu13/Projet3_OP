053afcba0c0f2ea8cee0f2057d0635d32da77f98
Alvindu13 <alcaraz.jeremie@hotmail.fr>
2018-10-18 16:16:49 +0200

    clean somes defaults in the Main Class -> devMode method.

--- src/main/java/com/jeremie/Main.java
+++ src/main/java/com/jeremie/Main.java
@@ -9,7 +9,7 @@ public class Main {
     /**
      * Run program.
      * @param args dev mode (add parameters args to choice dev mode or not. The dev mode args is priotary on dev mode of properties file).
-     * edit configuration parameters args : true = dev mode on and false = dev mode off.
+     * edit configuration program args : true = dev mode on and false = dev mode off.
      */
     public static void main(String[] args) {
         String[] devMode = new String[1];
@@ -20,9 +20,7 @@ public class Main {
         }catch (ArrayIndexOutOfBoundsException e){
             noArgs = true;
         }
-        System.out.println(noArgs);
         devMode = devMode(devMode, noArgs, args);
-        System.out.println(devMode[0]);
         game = new GameSelector(Boolean.parseBoolean(devMode[0]));
         game.numberRun();
     }
@@ -40,20 +38,17 @@ public class Main {
         try {
             prop.load(new FileReader("src/main/resources/config.properties"));
             propertiesDevMode = prop.getProperty("dev.mode");
-            System.out.println(propertiesDevMode);
-            System.out.println(noArgs);
             if(propertiesDevMode == null && noArgs == true)
                 devMode[0] = "false";
             else if(noArgs == false)
                 devMode[0] = args[0];
-            else if((propertiesDevMode == "true" || propertiesDevMode == "false") && noArgs == true)
+            else if((propertiesDevMode.equals("true") || (propertiesDevMode.equals("false")) && noArgs == true))
                 devMode[0] = propertiesDevMode;
             else
                 devMode[0] = "false";
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println(devMode[0]);
         return devMode;
     }
 }
